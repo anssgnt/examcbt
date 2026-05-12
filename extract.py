3edd437be26ce4cac8ec7241426beb0b7838cb04
anssgnt <59947648+anssgnt@users.noreply.github.com>
2026-05-12 13:28:21 +0700

    delete

--- extract.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('supabase-patch.min.js', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-start = text.find('if("getAdminMonitoringData"===a)')
-if start != -1:
-    print(text[start:start+1000])
